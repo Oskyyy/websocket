7267755651d06658cd798fc3035c500cbafbd772
Oskyyy <oskarwro@gmail.com>
2020-02-02 21:24:02 +0100

    Finished first steps - Client

---
+++ client/app.js
@@ -0,0 +1,54 @@
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
+
+let userName = '';
+
+login = (event) => {
+    event.preventDefault();
+    if(!userNameInput.value){
+        event.preventDefault();
+        alert('Enter your name'); 
+    } else{
+        userName = userNameInput.value;
+        loginForm.classList.remove('show');
+        messagesSection.classList.add('show');
+    };    
+};
+
+function addMessage(author, content) {
+    const message = document.createElement('li');
+    message.classList.add('message');
+    message.classList.add('message--received');
+    if(author === userName) message.classList.add('message--self');
+    message.innerHTML = `
+      <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+      <div class="message__content">
+        ${content}
+      </div>
+    `;
+    messagesList.appendChild(message);
+  }
+
+sendMessage = event => {
+    event.preventDefault();
+
+    if(!messageContentInput.value){
+        alert('Enter message before U send');
+    }  else{
+        addMessage(userName, messageContentInput.value);
+        messageContentInput.value = '';
+    };
+};
+
+loginForm.addEventListener('submit', function(event){
+    login(event);
+});
+
+addMessageForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    sendMessage(event);
+});
